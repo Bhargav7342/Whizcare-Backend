084a144267b3e0c8c4a89f22e116ad8af88a5a2b
Bhargav7342 <BhargavBhat7342@gmail.com>
2023-04-09 18:16:45 +0530

    Project shifted to personal repo

---
+++ Allergy_Entities/Entities/AllergyDContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataEntities.Entities;
+
+public partial class AllergyDContext : DbContext
+{
+    public AllergyDContext()
+    {
+    }
+
+    public AllergyDContext(DbContextOptions<AllergyDContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Allergy> Allergies { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Allergy>(entity =>
+        {
+            entity.HasKey(e => e.AllergyId).HasName("PK__Allergy__5855FEB7877947C2");
+
+            entity.ToTable("Allergy");
+
+            entity.Property(e => e.AllergyId)
+                .HasDefaultValueSql("(newid())")
+                .HasColumnName("Allergy_Id");
+            entity.Property(e => e.AllergyName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("Allergy_Name");
+            entity.Property(e => e.AppointmentId).HasColumnName("Appointment_Id");
+        });
+
+       
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
